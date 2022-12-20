7603caab6d575a8890e3fe5f5b2373a6da376024
HoaBanMC <losammc@gmail.com>
2022-12-20 16:09:19 +0700

    fix: public-api

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LettersAvatarComponent } from 'src/app/modules/letters-avatar/letters-avatar.component';
+import { LettersAvatarComponent } from './src/app/modules/letters-avatar/letters-avatar.component';
 
-export * from 'src/app/modules/letters-avatar/letters-avatar.component';
+export * from './src/app/modules/letters-avatar/letters-avatar.component';
 
 @NgModule({
   imports: [
